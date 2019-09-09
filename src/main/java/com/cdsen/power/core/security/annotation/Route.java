55329df5f14ded484a6d9dc85370438c1823592c
unknown <1178515826@qq.com>
2019-09-09 16:07:49 +0800

    新增Route和SecurityModule注解

---
+++ src/main/java/com/cdsen/power/core/security/annotation/Route.java
@@ -0,0 +1,16 @@
+package com.cdsen.power.core.security.annotation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+/**
+ * @author HuSen
+ * create on 2019/9/9 16:00
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Inherited
+@Documented
+public @interface Route {
+}
---
+++ src/main/java/com/cdsen/power/core/security/annotation/SecurityModule.java
@@ -0,0 +1,17 @@
+package com.cdsen.power.core.security.annotation;
+
+import java.lang.annotation.*;
+
+/**
+ * @author HuSen
+ * create on 2019/7/18 10:35
+ */
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface SecurityModule {
+
+    String name() default "";
+
+    Route route();
+}
