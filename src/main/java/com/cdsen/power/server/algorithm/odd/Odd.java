4ffd9a9d1905429d39110557924ca5945d4f9aa9
unknown <1178515826@qq.com>
2019-11-26 18:38:46 +0800

    ok

--- src/main/java/com/cdsen/power/server/algorithm/odd/Odd.java
+++ src/main/java/com/cdsen/power/server/algorithm/odd/Odd.java
@@ -3,6 +3,9 @@ package com.cdsen.power.server.algorithm.odd;
 /**
  * 是否为奇数
  *
+ * % 该操作符被定义为所有的int数值a和所有的非零int数值b，都满足下面的恒等式：
+ * (a / b) * b + (a % b) == a
+ * 意味着：当取余操作返回一个非零的结果时，它与左操作数具有相同的正负符号。
  * @author HuSen
  * create on 2019/11/26 10:48
  */
