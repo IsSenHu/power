d4d34167d5b967e9b1bb05bd971a32f30646bfa3
unknown <1178515826@qq.com>
2019-12-03 11:46:31 +0800

    ok

---
+++ src/main/java/com/cdsen/power/server/plan/controller/PlanController.java
@@ -0,0 +1,10 @@
+package com.cdsen.power.server.plan.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/plan")
+public class PlanController {
+
+}
