1b126aa7074584a3dd7a651c574b72722b6ef0f7
unknown <1178515826@qq.com>
2019-12-03 12:10:16 +0800

    ok

--- src/main/java/com/cdsen/power/server/plan/controller/PlanController.java
+++ src/main/java/com/cdsen/power/server/plan/controller/PlanController.java
@@ -3,6 +3,10 @@ package com.cdsen.power.server.plan.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * @author HuSen
+ * create on 2019/12/2 14:17
+ */
 @RestController
 @RequestMapping("/api/plan")
 public class PlanController {
