4d139238cdabcb2e66bfae2d26b86ca12366d130
unknown <1178515826@qq.com>
2019-12-13 17:36:59 +0800

    ok

--- src/main/java/com/cdsen/power/server/algorithm/sort/impl/Bubble.java
+++ src/main/java/com/cdsen/power/server/algorithm/sort/impl/Bubble.java
@@ -1,21 +1,22 @@
 package com.cdsen.power.server.algorithm.sort.impl;
 
+import com.cdsen.power.server.algorithm.sort.AbstractSortExample;
+
 /**
  * 冒泡排序
  *
  * @author HuSen
  * create on 2019/12/12 18:16
  */
-public class Bubble {
+public class Bubble extends AbstractSortExample {
 
-    public static void sort(int[] arr) {
+    @Override
+    public void sort(int[] arr) {
         int length = arr.length;
         for (int i = 0; i < length - 1; i++) {
             for (int j = i + 1; j < length; j++) {
-                if (arr[i] > arr[j]) {
-                    int temp = arr[i];
-                    arr[i] = arr[j];
-                    arr[j] = temp;
+                if (less(arr[j], arr[i])) {
+                    each(arr, i, j);
                 }
             }
         }
