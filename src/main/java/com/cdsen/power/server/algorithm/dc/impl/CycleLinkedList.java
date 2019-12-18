165271605b709d781c6d8dbaca808da33ab44d30
unknown <1178515826@qq.com>
2019-12-18 17:55:00 +0800

    è循环链表和双向链表待实现

---
+++ src/main/java/com/cdsen/power/server/algorithm/dc/impl/CycleLinkedList.java
@@ -0,0 +1,45 @@
+package com.cdsen.power.server.algorithm.dc.impl;
+
+import com.cdsen.power.server.algorithm.dc.interfaces.List;
+
+/**
+ * @author HuSen
+ * create on 2019/12/18 17:53
+ */
+public class CycleLinkedList<T> implements List<T> {
+
+    @Override
+    public boolean listEmpty() {
+        return false;
+    }
+
+    @Override
+    public void clearList() {
+
+    }
+
+    @Override
+    public T getElem(int i) {
+        return null;
+    }
+
+    @Override
+    public int locateElem(T e) {
+        return 0;
+    }
+
+    @Override
+    public void listInsert(int i, T e) {
+
+    }
+
+    @Override
+    public T listDelete(int i) {
+        return null;
+    }
+
+    @Override
+    public int listLength() {
+        return 0;
+    }
+}
